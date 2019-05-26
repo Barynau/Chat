c84d8208b1df940423a81a163470d297e21b596e
Dzmitry Barynov <BarynauDA@gmail.com>
2019-05-26 21:38:32 +0300

    Delete ConnectionListener.java

--- Network/src/main/java/ConnectionListener.java
+++
@@ -1,8 +0,0 @@
-public interface ConnectionListener {
-    void connectionReady(Connection connection);
-    void onReciveString(Connection connection,String string);
-    void onDisconnect(Connection connection);
-    void onExeption (Connection connection,Exception e);
-
-
-}
